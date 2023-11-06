857a9467a63c4afdde6d539b724037e57cb25ad7
Andreas Pelme <andreas@pelme.se>
2023-11-06 11:03:46 +0100

    Better test for kwargs.

--- tests/test_attributes.py
+++ tests/test_attributes.py
@@ -92,5 +92,5 @@ def test_id_class_wrong_order() -> None:
 
 
 def test_id_class_and_kwargs() -> None:
-    result = div("#theid", for_="hello")
-    assert str(result) == """<div id="theid" for="hello"></div>"""
+    result = div("#theid", for_="hello", data_foo="<bar")
+    assert str(result) == """<div id="theid" for="hello" data-foo="&lt;bar"></div>"""
