40ad20547ee48593ff594ab7341073d10b75a36a
Andreas Pelme <andreas@pelme.se>
2024-03-08 21:22:03 +0100

    use usefixtures in django test

--- tests/test_django.py
+++ tests/test_django.py
@@ -1,3 +1,4 @@
+import pytest
 from django.forms.utils import ErrorList
 from django.template import Context, Template
 from django.utils.html import escape as django__escape
@@ -5,24 +6,26 @@ from django.utils.safestring import SafeString as django__SafeString
 
 from htpy import div, li, ul
 
+pytestmark = pytest.mark.usefixtures("django_env")
 
-def test_template_injection(django_env: None) -> None:
+
+def test_template_injection() -> None:
     t = Template("<ul>{{ stuff }}</ul>")
     result = t.render(Context({"stuff": li["I am safe!"]}))
 
     assert result == "<ul><li>I am safe!</li></ul>"
 
 
-def test_SafeString(django_env: None) -> None:
+def test_SafeString() -> None:
     result = ul[django__SafeString("<li>hello</li>")]
     assert str(result) == "<ul><li>hello</li></ul>"
 
 
-def test_explicit_escape(django_env: None) -> None:
+def test_explicit_escape() -> None:
     result = ul[django__escape("<hello>")]
     assert str(result) == "<ul>&lt;hello&gt;</ul>"
 
 
-def test_errorlist(django_env: None) -> None:
+def test_errorlist() -> None:
     result = div[ErrorList(["my error"])]
     assert str(result) == """<div><ul class="errorlist"><li>my error</li></ul></div>"""
