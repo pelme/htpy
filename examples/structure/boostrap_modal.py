ffe14371406e7316ce3af9eb2e173981c9289064
Andreas Pelme <andreas@pelme.se>
2024-03-09 16:16:18 +0100

    lint+format examples

--- examples/structure/boostrap_modal.py
+++ examples/structure/boostrap_modal.py
@@ -25,15 +25,16 @@ def bs_modal(*, title, body=None, footer=None):
     ]
 
 
-from htpy import p
+if __name__ == "__main__":
+    from htpy import p
 
-print(
-    bs_modal(
-        title="Modal title",
-        body=p["Modal body text goes here."],
-        footer=[
-            button(".btn.btn-primary", type="button")["Save changes"],
-            button(".btn.btn-secondary", type="button")["Close"],
-        ],
+    print(
+        bs_modal(
+            title="Modal title",
+            body=p["Modal body text goes here."],
+            footer=[
+                button(".btn.btn-primary", type="button")["Save changes"],
+                button(".btn.btn-secondary", type="button")["Close"],
+            ],
+        )
     )
-)
