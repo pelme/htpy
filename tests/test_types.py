220598b9fdc207b0428d301eae751e200ec7348b
Andreas Pelme <andreas@pelme.se>
2024-03-10 22:19:15 +0100

    improved typing

--- tests/test_types.py
+++ tests/test_types.py
@@ -1,6 +1,6 @@
 from typing import assert_type
 
-from htpy import Element, div
+from htpy import Element, div, li, ul
 
 
 def test_element_type() -> None:
@@ -12,3 +12,13 @@ def test_element_type() -> None:
 def test_html_safestring_interface() -> None:
     result = str(div(id="a")).__html__()  # type: ignore[attr-defined]
     assert result == '<div id="a"></div>'
+
+
+class Test_Children:
+    def test_children_as_element(self) -> None:
+        child: Element = li
+        ul[child]
+
+    def test_children_as_list_element(self) -> None:
+        child: list[Element] = [div]
+        div[child]
