8fce40737727070b31e0737804186b4aee0deaff
Andreas Pelme <andreas@pelme.se>
2024-03-12 20:16:33 +0100

    remove redundant test

--- tests/test_jinja2.py
+++ tests/test_jinja2.py
@@ -1,14 +1,8 @@
 from jinja2 import Template
-from markupsafe import escape
 
-from htpy import li, ul
+from htpy import li
 
 
 def test_template_injection() -> None:
     result = Template("<ul>{{ stuff }}</ul>").render(stuff=li["I am safe!"])
     assert result == "<ul><li>I am safe!</li></ul>"
-
-
-def test_explicit_escape() -> None:
-    result = ul[escape("<hi>")]
-    assert str(result) == "<ul>&lt;hi&gt;</ul>"
