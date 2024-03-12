2ea924d9d9621808ac26c317a59b57a8e4e0628b
Andreas Pelme <andreas@pelme.se>
2024-03-12 20:25:40 +0100

    remove redundant test

--- tests/test_children.py
+++ tests/test_children.py
@@ -101,29 +101,6 @@ def test_ignore_none() -> None:
 
 
 def test_iter() -> None:
-    result = div(id="a")["hello"]
-    parts = list(result)
-
-    assert parts == ['<div id="a">', "hello", "</div>"]
-
-
-def test_iter_nested() -> None:
-    result = ul[li(id="a")["hi a"], li(id="b")["hi b"]]
-    parts = list(result)
-
-    assert parts == [
-        "<ul>",
-        '<li id="a">',
-        "hi a",
-        "</li>",
-        '<li id="b">',
-        "hi b",
-        "</li>",
-        "</ul>",
-    ]
-
-
-def test_generator() -> None:
     trace = "not started"
 
     def generate_list() -> Generator[Element, None, None]:
