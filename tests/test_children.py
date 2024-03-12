10a3a69e892232af1eeac3753026efb97ec475d5
Andreas Pelme <andreas@pelme.se>
2024-03-12 19:51:21 +0100

    move lazy tests

--- tests/test_children.py
+++ tests/test_children.py
@@ -3,7 +3,7 @@ from typing import assert_type
 
 import pytest
 
-from htpy import Element, dd, div, dl, dt, html, img, input, li, my_custom_element, ul
+from htpy import Element, VoidElement, dd, div, dl, dt, html, img, input, li, my_custom_element, ul
 
 
 def test_void_element() -> None:
@@ -118,3 +118,45 @@ def test_iter_nested() -> None:
         "</li>",
         "</ul>",
     ]
+
+
+def test_generator() -> None:
+    trace = "not started"
+
+    def generate_list() -> Generator[Element, None, None]:
+        nonlocal trace
+
+        trace = "before yield"
+        yield li("#a")
+
+        trace = "done"
+
+    iterator = iter(ul[generate_list()])
+
+    assert next(iterator) == "<ul>"
+    assert trace == "not started"
+
+    assert next(iterator) == '<li id="a">'
+    assert trace == "before yield"
+    assert next(iterator) == "</li>"
+    assert trace == "before yield"
+
+    assert next(iterator) == "</ul>"
+    assert trace == "done"
+
+
+def test_callable() -> None:
+    called = False
+
+    def generate_img() -> VoidElement:
+        nonlocal called
+        called = True
+        return img
+
+    iterator = iter(div[generate_img])
+
+    assert next(iterator) == "<div>"
+    assert called is False
+    assert next(iterator) == "<img>"
+    assert called is True
+    assert next(iterator) == "</div>"
