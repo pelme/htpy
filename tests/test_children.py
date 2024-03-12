c36284e8bfb93ebfbaeb2235b3a3e2f99f78705e
Andreas Pelme <andreas@pelme.se>
2024-03-12 19:49:39 +0100

    move test_iter

--- tests/test_children.py
+++ tests/test_children.py
@@ -95,3 +95,26 @@ def test_custom_element() -> None:
 
 def test_ignore_none() -> None:
     assert str(div[None]) == "<div></div>"
+
+
+def test_iter() -> None:
+    result = div(id="a")["hello"]
+    parts = list(result)
+
+    assert parts == ['<div id="a">', "hello", "</div>"]
+
+
+def test_iter_nested() -> None:
+    result = ul[li(id="a")["hi a"], li(id="b")["hi b"]]
+    parts = list(result)
+
+    assert parts == [
+        "<ul>",
+        '<li id="a">',
+        "hi a",
+        "</li>",
+        '<li id="b">',
+        "hi b",
+        "</li>",
+        "</ul>",
+    ]
