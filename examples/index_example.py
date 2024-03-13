c5a850542d57e2b70571b4f42ce33ffafbf41690
Andreas Pelme <andreas@pelme.se>
2024-03-13 21:29:38 +0100

    add print to example file

--- examples/index_example.py
+++ examples/index_example.py
@@ -2,9 +2,11 @@ from htpy import body, h1, html, img
 
 is_cool = True
 
-html[
-    body(class_={"cool": is_cool})[
-        h1("#hi")["Welcome to htpy!"],
-        img(src="grumpycat.jpg"),
+print(
+    html[
+        body(class_={"cool": is_cool})[
+            h1("#hi")["Welcome to htpy!"],
+            img(src="cat.jpg"),
+        ]
     ]
-]
+)
