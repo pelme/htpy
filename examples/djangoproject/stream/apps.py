22083dd4d752447ea7a171b6b0e426c8cfb49801
Andreas Pelme <andreas@pelme.se>
2024-03-19 22:53:11 +0100

    ruff format

--- examples/djangoproject/stream/apps.py
+++ examples/djangoproject/stream/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class StreamConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'stream'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "stream"
