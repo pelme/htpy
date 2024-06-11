3cf0827b253632e3a00c8e7733025dc5816622ad
Ole-Jakob Olsen <ole.jakob.olsen@ngi.no>
2024-06-11 18:36:23 +0200

    Undo unintentional formatting of __init__.py

--- htpy/__init__.py
+++ htpy/__init__.py
@@ -97,9 +97,7 @@ def _generate_attrs(raw_attrs: dict[str, Attribute]) -> Iterable[tuple[str, Attr
 
 
 def _attrs_string(attrs: dict[str, Attribute]) -> str:
-    result = " ".join(
-        k if v is True else f'{k}="{v}"' for k, v in _generate_attrs(attrs)
-    )
+    result = " ".join(k if v is True else f'{k}="{v}"' for k, v in _generate_attrs(attrs))
 
     if not result:
         return ""
@@ -151,10 +149,7 @@ class BaseElement:
 
     @overload
     def __call__(
-        self: BaseElementSelf,
-        id_class: str,
-        attrs: dict[str, Attribute],
-        **kwargs: Attribute,
+        self: BaseElementSelf, id_class: str, attrs: dict[str, Attribute], **kwargs: Attribute
     ) -> BaseElementSelf: ...
     @overload
     def __call__(
@@ -230,9 +225,7 @@ class _HasHtml(Protocol):
 
 _ClassNamesDict: TypeAlias = dict[str, bool]
 _ClassNames: TypeAlias = Iterable[str | None | bool | _ClassNamesDict] | _ClassNamesDict
-Node: TypeAlias = (
-    None | str | BaseElement | _HasHtml | Iterable["Node"] | Callable[[], "Node"]
-)
+Node: TypeAlias = None | str | BaseElement | _HasHtml | Iterable["Node"] | Callable[[], "Node"]
 
 Attribute: TypeAlias = None | bool | str | _HasHtml | _ClassNames
 
