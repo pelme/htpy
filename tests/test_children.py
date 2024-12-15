pelme/htpy

commit 909ecb8cc1430a9ea776c5b0ce9f12e161c9671c

Better error handling for invalid children.
Validate children directly when possible.

htpy/__init__.py
@@ -6,10 +6,13 @@ __all__: list[str] = []
 import dataclasses
 import functools
 import typing as t
-from collections.abc import Callable, Iterable, Iterator
+from collections.abc import Callable, Generator, Iterable, Iterator
 
 from markupsafe import Markup as _Markup
 from markupsafe import escape as _escape
+
+if t.TYPE_CHECKING:
+    from types import UnionType
 
 BaseElementSelf = t.TypeVar("BaseElementSelf", bound="BaseElement")
 ElementSelf = t.TypeVar("ElementSelf", bound="Element")
@@ -190,7 +193,7 @@ def _iter_node_context(x: Node, context_dict: dict[Context[t.Any], t.Any]) -> It
         yield str(_escape(x))
     elif isinstance(x, int):
         yield str(x)
-    elif isinstance(x, Iterable) and not isinstance(x, bytes):  # pyright: ignore [reportUnnecessaryIsInstance]
+    elif isinstance(x, Iterable) and not isinstance(x, _KnownInvalidChildren):  # pyright: ignore [reportUnnecessaryIsInstance]
         for child in x:
             yield from _iter_node_context(child, context_dict)
     else:
@@ -288,9 +291,22 @@ class BaseElement:
     do_not_call_in_templates = True
 
 
+def _validate_children(children: t.Any) -> None:
+    if isinstance(children, _KnownValidChildren):
+        return
+
+    if isinstance(children, Iterable) and not isinstance(children, _KnownInvalidChildren):
+        for child in children:  # pyright: ignore [reportUnknownVariableType]
+            _validate_children(child)
+        return
+
+    raise ValueError(f"{children!r} is not a valid child element")
+
+
 class Element(BaseElement):
     def __getitem__(self: ElementSelf, children: Node) -> ElementSelf:
-        return self.__class__(self._name, self._attrs, children)
+        _validate_children(children)
+        return self.__class__(self._name, self._attrs, children)  # pyright: ignore [reportUnknownArgumentType]
 
 
 class HTMLElement(Element):
@@ -457,3 +473,18 @@ tr = Element("tr")
 u = Element("u")
 ul = Element("ul")
 var = Element("var")
+
+
+_KnownInvalidChildren: UnionType = bytes | bytearray | memoryview
+
+_KnownValidChildren: UnionType = (  # pyright: ignore [reportUnknownVariableType]
+    None
+    | BaseElement
+    | ContextProvider  # pyright: ignore [reportMissingTypeArgument]
+    | ContextConsumer  # pyright: ignore [reportMissingTypeArgument]
+    | Callable  # pyright: ignore [reportMissingTypeArgument]
+    | str
+    | int
+    | Generator  # pyright: ignore [reportMissingTypeArgument]
+    | _HasHtml
+)

tests/test_children.py
@@ -1,5 +1,10 @@
 from __future__ import annotations
 
+import dataclasses
+import datetime
+import decimal
+import pathlib
+import re
 import typing as t
 
 import pytest
@@ -202,7 +207,68 @@ def test_callable_in_generator() -> None:
     assert str(div[((lambda: "hi") for _ in range(1))]) == "<div>hi</div>"
 
 
-@pytest.mark.parametrize("not_a_child", [12.34, b"foo", object(), object])
-def test_invalid_child(not_a_child: t.Any) -> None:
+@dataclasses.dataclass
+class MyDataClass:
+    name: str
+
+
+class SomeClass:
+    pass
+
+
+# Various types that are not valid children.
+_invalid_children = [
+    12.34,
+    decimal.Decimal("12.34"),
+    complex("+1.23"),
+    object(),
+    datetime.date(1, 2, 3),
+    datetime.datetime(1, 2, 3),
+    datetime.time(1, 2),
+    b"foo",
+    bytearray(b"foo"),
+    memoryview(b"foo"),
+    Exception("foo"),
+    Ellipsis,
+    re.compile("foo"),
+    pathlib.Path("FOO"),
+    re,  # module type
+    MyDataClass(name="Andreas"),
+    SomeClass(),
+]
+
+
+@pytest.mark.parametrize("not_a_child", _invalid_children)
+def test_invalid_child_direct(not_a_child: t.Any) -> None:
     with pytest.raises(ValueError, match="is not a valid child element"):
-        str(div[not_a_child])
+        div[not_a_child]
+
+
+@pytest.mark.parametrize("not_a_child", _invalid_children)
+def test_invalid_child_nested_iterable(not_a_child: t.Any) -> None:
+    with pytest.raises(ValueError, match="is not a valid child element"):
+        div[[not_a_child]]
+
+
+@pytest.mark.parametrize("not_a_child", _invalid_children)
+def test_invalid_child_lazy_callable(not_a_child: t.Any) -> None:
+    """
+    Ensure proper exception is raised for lazily evaluated invalid children.
+    """
+    element = div[lambda: not_a_child]
+    with pytest.raises(ValueError, match="is not a valid child element"):
+        str(element)
+
+
+@pytest.mark.parametrize("not_a_child", _invalid_children)
+def test_invalid_child_lazy_generator(not_a_child: t.Any) -> None:
+    """
+    Ensure proper exception is raised for lazily evaluated invalid children.
+    """
+
+    def gen() -> t.Any:
+        yield not_a_child
+
+    element = div[gen()]
+    with pytest.raises(ValueError, match="is not a valid child element"):
+        str(element)

commit f0d173d4fb7f70bbcccb436382944b277dd83d62

Make possible to use one-shot iterators as children nodes.
Previously there were checks for generators specifically and not iterators in
general. We also support one-off iterators that are not based on generators such
as itertools.chain().

This regression was introduced in https://github.com/pelme/htpy/pull/56.

Based on https://github.com/pelme/htpy/pull/71.

htpy/__init__.py
@@ -4,7 +4,7 @@ import dataclasses
 import functools
 import keyword
 import typing as t
-from collections.abc import Callable, Generator, Iterable, Iterator
+from collections.abc import Callable, Iterable, Iterator
 
 from markupsafe import Markup as _Markup
 from markupsafe import escape as _escape
@@ -297,14 +297,26 @@ class BaseElement:
 
 
 def _validate_children(children: t.Any) -> None:
-    if isinstance(children, _KnownValidChildren):
-        return
-
-    if isinstance(children, Iterable) and not isinstance(children, _KnownInvalidChildren):
-        for child in children:  # pyright: ignore [reportUnknownVariableType]
+    # Non-lazy iterables:
+    # list and tuple are iterables and part of _KnownValidChildren. Since we
+    # know they can be consumed multiple times, we validate them recursively now
+    # rather than at render time to provide better error messages.
+    if isinstance(children, list | tuple):
+        for child in children:  # pyright: ignore[reportUnknownVariableType]
             _validate_children(child)
         return
 
+    # bytes, bytearray etc:
+    # These are Iterable (part of _KnownValidChildren) but still not
+    # useful as a child node.
+    if isinstance(children, _KnownInvalidChildren):
+        raise TypeError(f"{children!r} is not a valid child element")
+
+    # Element, str, int and all other regular/valid types.
+    if isinstance(children, _KnownValidChildren):
+        return
+
+    # Arbitrary objects that are not valid children.
     raise TypeError(f"{children!r} is not a valid child element")
 
 
@@ -487,15 +499,14 @@ var = Element("var")
 
 
 _KnownInvalidChildren: UnionType = bytes | bytearray | memoryview
-
-_KnownValidChildren: UnionType = (  # pyright: ignore [reportUnknownVariableType]
+_KnownValidChildren: UnionType = (
     None
     | BaseElement
     | ContextProvider  # pyright: ignore [reportMissingTypeArgument]
     | ContextConsumer  # pyright: ignore [reportMissingTypeArgument]
-    | Callable  # pyright: ignore [reportMissingTypeArgument]
     | str
     | int
-    | Generator  # pyright: ignore [reportMissingTypeArgument]
     | _HasHtml
+    | Callable
+    | Iterable
 )

tests/test_children.py
@@ -6,6 +6,7 @@ import decimal
 import pathlib
 import re
 import typing as t
+from collections.abc import Iterator
 
 import pytest
 from markupsafe import Markup
@@ -16,11 +17,34 @@ from htpy import Element, VoidElement, dd, div, dl, dt, html, img, input, li, my
 from .conftest import Trace
 
 if t.TYPE_CHECKING:
-    from collections.abc import Callable, Generator
+    from collections.abc import Callable
 
     from htpy import Node
 
     from .conftest import RenderFixture, TraceFixture
+
+
+T = t.TypeVar("T")
+
+
+class SingleShotIterator(Iterator[T]):
+    def __init__(self, value: T, trace: TraceFixture = lambda x: None) -> None:
+        self.value = value
+        self.trace = trace
+        self.consumed = False
+
+    def __iter__(self) -> SingleShotIterator[T]:
+        return self
+
+    def __next__(self) -> T:
+        if self.consumed:
+            self.trace("SingleShotIterator: StopIteration")
+            raise StopIteration
+
+        self.consumed = True
+        self.trace("SingleShotIterator: returning value")
+
+        return self.value
 
 
 def test_void_element(render: RenderFixture) -> None:
@@ -88,12 +112,12 @@ def test_flatten_very_nested_children(render: RenderFixture) -> None:
 
 
 def test_flatten_nested_generators(render: RenderFixture) -> None:
-    def cols() -> Generator[str, None, None]:
+    def cols() -> Iterator[str]:
         yield "a"
         yield "b"
         yield "c"
 
-    def rows() -> Generator[Generator[str, None, None], None, None]:
+    def rows() -> Iterator[Iterator[str]]:
         yield cols()
         yield cols()
         yield cols()
@@ -104,9 +128,21 @@ def test_flatten_nested_generators(render: RenderFixture) -> None:
 
 
 def test_generator_children(render: RenderFixture) -> None:
-    gen: Generator[Element, None, None] = (li[x] for x in ["a", "b"])
+    gen: Iterator[Element] = (li[x] for x in ["a", "b"])
     result = ul[gen]
     assert render(result) == ["<ul>", "<li>", "a", "</li>", "<li>", "b", "</li>", "</ul>"]
+
+
+def test_non_generator_iterator(render: RenderFixture, trace: TraceFixture) -> None:
+    result = div[SingleShotIterator("hello", trace=trace)]
+
+    assert render(result) == [
+        "<div>",
+        Trace("SingleShotIterator: returning value"),
+        "hello",
+        Trace("SingleShotIterator: StopIteration"),
+        "</div>",
+    ]
 
 
 def test_html_tag_with_doctype(render: RenderFixture) -> None:
@@ -137,7 +173,7 @@ def test_ignored(render: RenderFixture, ignored_value: t.Any) -> None:
 
 
 def test_lazy_iter(render: RenderFixture, trace: TraceFixture) -> None:
-    def generate_list() -> Generator[Element, None, None]:
+    def generate_list() -> Iterator[Element]:
         trace("before yield")
         yield li("#a")
         trace("after yield")
@@ -202,7 +238,7 @@ def test_safe_children(render: RenderFixture) -> None:
 
 
 def test_nested_callable_generator(render: RenderFixture) -> None:
-    def func() -> Generator[str, None, None]:
+    def func() -> Iterator[str]:
         return (x for x in "abc")
 
     assert render(div[func]) == ["<div>", "a", "b", "c", "</div>"]
@@ -260,7 +296,19 @@ def test_invalid_child_direct(not_a_child: t.Any) -> None:
 
 
 @pytest.mark.parametrize("not_a_child", _invalid_children)
-def test_invalid_child_nested_iterable(not_a_child: t.Any) -> None:
+def test_invalid_child_wrapped_in_list(not_a_child: t.Any) -> None:
+    with pytest.raises(TypeError, match="is not a valid child element"):
+        div[[not_a_child]]
+
+
+@pytest.mark.parametrize("not_a_child", _invalid_children)
+def test_invalid_child_wrapped_in_tuple(not_a_child: t.Any) -> None:
+    with pytest.raises(TypeError, match="is not a valid child element"):
+        div[(not_a_child,)]
+
+
+@pytest.mark.parametrize("not_a_child", _invalid_children)
+def test_invalid_child_nested_iterator(not_a_child: t.Any) -> None:
     with pytest.raises(TypeError, match="is not a valid child element"):
         div[[not_a_child]]
 
@@ -276,14 +324,11 @@ def test_invalid_child_lazy_callable(not_a_child: t.Any, render: RenderFixture) 
 
 
 @pytest.mark.parametrize("not_a_child", _invalid_children)
-def test_invalid_child_lazy_generator(not_a_child: t.Any, render: RenderFixture) -> None:
+def test_invalid_child_lazy_iterator(not_a_child: t.Any, render: RenderFixture) -> None:
     """
     Ensure proper exception is raised for lazily evaluated invalid children.
     """
 
-    def gen() -> t.Any:
-        yield not_a_child
-
-    element = div[gen()]
+    element = div[SingleShotIterator(not_a_child)]
     with pytest.raises(TypeError, match="is not a valid child element"):
         render(element)